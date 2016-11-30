e184ad6b7623cb5ee5cdc18c3ecc2d42ba77ebbf
ampleeve <e@ampleev.com>
2016-11-30 16:58:40 +0300

    fsdfn

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: evgenijampleev
- * Date: 29.11.16
- * Time: 19:23
- */
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title>GAME</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<script language="javascript" src="script.js"></script>
+</head>
+<body>
+	<div id="matrix"></div>
+</body>
+</html>
+
+
+<!--<html>
+<head>
+	<script type="text/javascript">
+		function whichButton(event)
+		{
+			alert(event.keyCode);
+		}
+
+	</script>
+</head>
+
+<body onkeyup="whichButton(event)">
+
+<p>Нажмите на клавишу на клавиатуре. В всплывающем окне будет отображен код клавиши, которую вы нажали.</p>
+</body>
+
+</html>-->
